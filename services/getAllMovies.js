6277097f1132e09e2167d2d0aef61acc6db64389
dhanno75 <pdhananjay774@gmail.com>
2023-01-25 15:05:29 +0530

    first commit

--- services/getAllMovies.js
+++ services/getAllMovies.js
@@ -1,19 +1,26 @@
 import { client } from "../index.js";
+import { ObjectId } from "mongodb";
 
 export async function updateMovie(id, data) {
   return await client
     .db("test")
     .collection("movies")
-    .updateOne(id, { $set: data });
+    .updateOne({ _id: ObjectId(id) }, { $set: data });
 }
 export async function deleteMovie(id) {
-  return await client.db("test").collection("movies").deleteOne({ id });
+  return await client
+    .db("test")
+    .collection("movies")
+    .deleteOne({ _id: ObjectId(id) });
 }
 export async function addMovie(data) {
   return await client.db("test").collection("movies").insertMany(data);
 }
 export async function getMoviebyId(id) {
-  return await client.db("test").collection("movies").findOne({ id });
+  return await client
+    .db("test")
+    .collection("movies")
+    .findOne({ _id: ObjectId(id) });
 }
 export async function getAllMovies() {
   return await client.db("test").collection("movies").find({}).toArray();
